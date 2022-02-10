14d628d995c1e378e171f1d8744195553b77459a
Agus Priyatin <aguspriyatin80@gmail.com>
2022-02-10 08:53:09 +0700

    crud, validation, table relation

---
+++ src/main/java/com/domain/services/ProductService.java
@@ -0,0 +1,52 @@
+package com.domain.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import com.domain.models.entities.Product;
+import com.domain.models.entities.Supplier;
+import com.domain.models.repos.ProductRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ProductService {
+    @Autowired
+    private ProductRepo productRepo;
+    public Product save(Product product){ // bisa untuk SAVE dan UPDATE
+        return productRepo.save(product);        
+    }
+    public Product findOne(Long id){
+        Optional<Product> product = productRepo.findById(id);
+        if(!product.isPresent()) {
+            return null;
+        }
+        return product.get();
+    }
+
+    public Iterable<Product> findAll(){
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id){
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> findByName(String name){
+        return productRepo.findByNameContains(name);
+    }
+
+    public void addSupplier(Supplier supplier, Long productId){
+        Product product = findOne(productId);
+        if(product == null){
+            throw new RuntimeException("Product with ID "+productId+" not found");            
+        }
+        product.getSuppliers().add(supplier);
+        save(product);
+    }
+
+}
